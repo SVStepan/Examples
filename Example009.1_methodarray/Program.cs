a1403a9ddb2ba56107cea0371cfd0cac8325585e
Stepan <stepan_smirnoff@mail.ru>
2021-11-26 23:57:53 +0300

    exampl009

---
+++ Example009.1_methodarray/Program.cs
@@ -0,0 +1,67 @@
+﻿// int[] array = { 12, 21, 53, 84, 91, 75, 26, 17, 28, 91 };
+
+// int n = array.Length;
+// int find = 91;
+
+// int index = 0;
+
+// while (index < n)
+// {
+//     if (array[index] == find)
+//     {
+//         Console.WriteLine(index);
+//         break;
+//     }
+//     // index = index + 1;
+//     index++;
+// }
+
+void FillArray(int[] collection)
+{
+    int Length = collection.Length;
+    int index = 0;
+    while (index < Length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        // index = index + 1;
+        index++;
+
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 3);
+Console.WriteLine(pos);
---
+++ Example009_methodarray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = { 12, 21, 53, 84, 91, 75, 26, 17, 28, 91 };
+
+int n = array.Length;
+int find = 91;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    // index = index + 1;
+    index++;
+}
