44ff00da3eb4ef7cee43fc49afc5afb7c066f362
Stepan <stepan_smirnoff@mail.ru>
2021-11-27 23:54:32 +0300

    xamples12.1

--- Examples012_methods/Program.cs
+++ Examples012_methods/Program.cs
@@ -36,17 +36,83 @@ int Year = Method3();
 // Console.WriteLine(Year);
 
 // Вид 4 Что-то принимают и что-то возвращают
-string Method4(int count, string Text)
-{
-    int i = 0;
-    string result = String.Empty;
 
-    while (i<count)
+
+// string Method4(int count, string Text)
+// {
+//     int i = 0;
+//     string result = String.Empty;
+
+//     while (i<count)
+//     {
+//         result = result + Text;
+//         i++;
+//     }
+//     return result;
+// }
+// string res = Method4(10, "as_df");
+// Console.WriteLine(res);
+
+
+
+// string Method4(int count, string Text)
+// {
+//        string result = String.Empty;
+//     for (int i = 0; i<count; i++)
+//         {
+//         result = result + Text;
+        
+//     }
+//     return result;
+// }
+// string res = Method4(10, "as_df");
+// // Console.WriteLine(res);
+
+// for (int i = 2; i <= 10; i++)
+// {
+//     for (int j = 2; j <= 10; j++)
+//     {
+//         Console.WriteLine($"{i} * {j} = {i*j}");
+//     }
+//     Console.WriteLine();
+// }
+
+// ==== Работа с текстом
+//дан текст, в тексте нужно все пробелы заменить черточками,
+// маленькие буквы "к" заменить большими "К",
+// а большие "С" заменить на маленькие "с".
+
+// Яснали задача?
+
+string text = "-Я думаю, - сказал князь, улыбаясь, -что, "
+            + "ежели бы вас послали вместо нашегомилого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля."
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+// string s = "qwerty"
+//             01234
+// s[3] // r
+
+string Replase(string text, char oldValue, char newValue)
+{
+    string result = string.Empty;
+
+    int length = text.Length;
+    for (int i = 0; i< length; i++)
     {
-        result = result + Text;
-        i++;
+        if(text[i] == oldValue) result = result+ $"{newValue}";
+        else result = result + $"{text[i]}";
     }
+
     return result;
 }
-string res = Method4(10, "as_df");
-Console.WriteLine(res);
+string newText = Replase(text, ' ', '|');
+
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replase(newText, 'к', 'К');
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replase(newText, 'В', 'в');
+Console.WriteLine(newText);
+
