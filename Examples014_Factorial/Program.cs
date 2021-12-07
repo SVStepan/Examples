1b387cb3df8a0250a1da61ec77d6f0ea2cfc3d05
Stepan <stepan_smirnoff@mail.ru>
2021-12-07 22:42:00 +0300

    task14fib

---
+++ Examples014_Factorial/Program.cs
@@ -0,0 +1,31 @@
+﻿// // int Factorial(int n)
+// double Factorial(int n)
+// {
+//     if (n == 1) return 1;
+//     else return n * Factorial(n-1);
+// }
+// for (int i = 1; i < 40; i++)
+// {
+//     Console.WriteLine($"{i}! = {Factorial(i)}");
+// }
+
+
+
+// f(1) = 1
+// f(2) = 1
+// f(n) = f(n-1) + f(n-2)
+
+
+// double fibonacci(int n)
+// {
+//     if (n == 1 || n == 2) return 1;
+//     else return fibonacci(n-1) + fibonacci(n-2);
+// }
+
+// for (int i = 1; i < 50; i++)
+// {
+//     Console.WriteLine($"f({i}) = {fibonacci(i)}");
+// }
+
+
+
