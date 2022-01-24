19fa15ac47a3ad87e3128fbaeab582d09cf3317c
Stepan <stepan_smirnoff@mail.ru>
2022-01-24 22:49:50 +0300

    new

--- Examples016/Program.cs
+++ Examples016/Program.cs
@@ -1,3 +1,36 @@
-﻿
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+
+class Program
+{
+    static public void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
+
+
+
+
+// собрать строку от а до б 
+
+//сумма чисел от 1 до n
+
+// факториал числа
+
+// вычислить  а в степени n
+
+// перебор слов
+
+// решение, некоторых задач
+// алгоритм обхода дериктории 
+
+// string path = 
+// DirectoryInfo di = new DirectoryInfo();
+// System.Console.WriteLine(di.CreationTime);
+// FileInfo[]
+
 
 
